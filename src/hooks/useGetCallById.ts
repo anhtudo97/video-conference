d75bd2c61dd5a4184d5b4a97038ae28ddb7c7947
Alex Dou <38291962+anhtudo97@users.noreply.github.com>
2025-08-30 11:42:54 +0700

    feat: hooks for get call by id

---
+++ src/hooks/useGetCallById.ts
@@ -0,0 +1,36 @@
+'use client';
+
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+export const useGetCallById = (id: string | string[]) => {
+  const [call, setCall] = useState<Call>();
+  const [isCallLoading, setIsCallLoading] = useState(true);
+  const client = useStreamVideoClient();
+
+  useEffect(() => {
+    if (!client) return;
+
+    const loadCall = async () => {
+      try {
+        const { calls } = await client.queryCalls({ filter_conditions: { id } });
+        if (calls.length > 0) setCall(calls[0]);
+        setIsCallLoading(false);
+
+      } catch {
+        toast.error("Get call is errors");
+        setIsCallLoading(false);
+      }
+    };
+
+    loadCall();
+
+
+  }, [client, id]);
+
+  return { call, isCallLoading };
+
+}
+
+
